ab5a149c4749bf22c9f7427f1e5730e21fd51bc4
enochmalamba <enochmalamba@gmail.com>
2025-11-07 14:40:34 +0200

    feat: add MUI dependencies

--- src/pages/CreateHandle.jsx
+++ src/pages/CreateHandle.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 const CreateSocialPost = () => {
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
@@ -30,10 +31,45 @@ const CreateJob = () => {
   const [dueDate, setDueDate] = useState("");
   const [attachedFile, setAttachedFile] = useState("");
   const [employer, SetEmployer] = useState({}); // company name, image, about, etc.
+  const jTypes = [
+    {
+      label: "Full-time",
+      value: "full-time",
+    },
+    {
+      label: "Part-time",
+      value: "part-time",
+    },
+    {
+      label: "Contract",
+      value: "contract",
+    },
+    {
+      label: "Internship",
+      value: "internship",
+    },
+  ];
   return (
-    <div>
-      <h1>create job post here</h1>
-    </div>
+    <form>
+      <TextField
+        label="Job Position"
+        helperText="e.g. Sales Manager, Receiptionist, etc."
+        variant="outlined"
+        required
+      />
+      <TextField
+        label="Employment Type"
+        select
+        required
+        // helperText="Please select the type of employment"
+      >
+        {jTypes.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+    </form>
   );
 };
 function CreateHandle({ type = "job" }) {
